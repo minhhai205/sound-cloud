bbce03414a77174cde7d75a0a87b4a7fd669cfc8
minhhai205 <158561042+minhhai205@users.noreply.github.com>
2024-08-24 22:48:09 +0700

    update route

--- index.ts
+++ index.ts
@@ -2,15 +2,15 @@ import express, { Express, Request, Response } from "express";
 import env from "dotenv" 
 env.config();
 
+import route from "./routes/client/index.route";
+
 const app: Express = express();
 const port: (number | string) = `${process.env.PORT}` || 3000;
 
 app.set("views", "./views");
 app.set("view engine", "pug");
 
-app.get("/", (req: Request, res: Response) => {
-  res.render("client/pages/home/index");
-});
+route(app);
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
