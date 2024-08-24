9af5ba1c95345f902e5cc1380626ae34c105e1f9
minhhai205 <158561042+minhhai205@users.noreply.github.com>
2024-08-24 23:21:44 +0700

    add static file

--- index.ts
+++ index.ts
@@ -10,6 +10,8 @@ const port: (number | string) = `${process.env.PORT}` || 3000;
 app.set("views", "./views");
 app.set("view engine", "pug");
 
+app.use(express.static("public"));
+
 route(app);
 
 app.listen(port, () => {
