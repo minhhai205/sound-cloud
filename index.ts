cbd51e6719ca08bb3424781b8f85080922ed8c20
minhhai205 <158561042+minhhai205@users.noreply.github.com>
2024-08-24 17:54:16 +0700

    install pug

--- index.ts
+++ index.ts
@@ -1,1 +1,15 @@
-console.log("ok")
+import express, { Express, Request, Response } from "express";
+
+const app: Express = express();
+const port: number = 3000;
+
+app.set("views", "./views");
+app.set("view engine", "pug");
+
+app.get("/", (req: Request, res: Response) => {
+  res.render("client/pages/home/index");
+});
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
+});
