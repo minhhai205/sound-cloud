151d186629798837257a9a7387780fbba299295b
minhhai205 <158561042+minhhai205@users.noreply.github.com>
2024-08-24 22:56:48 +0700

    restructure pug folder

--- controllers/client/home.controller.ts
+++ controllers/client/home.controller.ts
@@ -2,5 +2,7 @@ import { Request, Response } from "express";
 
 // [GET] /
 export const index = async (req: Request, res: Response): Promise<void> => {
-  res.render("client/pages/home/index");
+  res.render("client/pages/home/index", {
+    pageTitle: "Trang chủ",
+  });
 }
