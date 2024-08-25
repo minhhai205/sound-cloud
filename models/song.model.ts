a83680f9670dbce976d418485abd39f60867dba2
minhhai205 <158561042+minhhai205@users.noreply.github.com>
2024-08-25 13:06:43 +0700

    create song, singer model

---
+++ models/song.model.ts
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+
+const songSchema = new mongoose.Schema(
+  {
+    title: String,
+    avatar: String,
+    description: String,
+    singerId: String,
+    topicId: String,
+    like: {
+      type: Number,
+      default: 0
+    },
+    lyrics: String,
+    audio: String,
+    status: String,
+    listen: {
+      type: Number,
+      default: 0
+    },
+    deleted: {
+      type: Boolean,
+      default: false,
+    },
+    deletedAt: Date,
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Song = mongoose.model("Song", songSchema, "songs");
+
+export default Song;
