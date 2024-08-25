c5e050035851d07b7228a83645f8aa2d534c5fc8
minhhai205 <158561042+minhhai205@users.noreply.github.com>
2024-08-25 14:28:21 +0700

    update home

--- controllers/client/home.controller.ts
+++ controllers/client/home.controller.ts
@@ -1,8 +1,15 @@
 import { Request, Response } from "express";
+import Playlist from "../../models/playlist.model";
 
 // [GET] /
 export const index = async (req: Request, res: Response): Promise<void> => {
+  const playlists = await Playlist.find({
+    deleted: false,
+    status: "active",
+  });
+
   res.render("client/pages/home/index", {
     pageTitle: "Trang chủ",
+    playlists: playlists,
   });
 }
