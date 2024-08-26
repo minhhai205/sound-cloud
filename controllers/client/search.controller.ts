337a0b72848e3dcfa3342ee3fd62796355fdcbb6
minhhai205 <158561042+minhhai205@users.noreply.github.com>
2024-08-27 00:19:53 +0700

    advanced search

--- controllers/client/search.controller.ts
+++ controllers/client/search.controller.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import Song from "../../models/song.model";
+import unidecode from "unidecode";
 
 // [GET] /search
 export const result = async (req: Request, res: Response): Promise<void> => {
@@ -7,8 +8,16 @@ export const result = async (req: Request, res: Response): Promise<void> => {
 
   const regexKeyword = new RegExp(keyword, "i");
   
+  const unidecodeText: string = unidecode(keyword);
+  const keywordSlug = unidecodeText.replace(/\s+/g, "-");
+  const keyWordSlugRegex = new RegExp(keywordSlug, "i");
+  
+
   const listSongs = await Song.find({
-    title: regexKeyword,
+    $or: [
+      { title: regexKeyword},
+      { slug: keyWordSlugRegex},
+    ],
     status: "public",
     deleted: false,
   });
