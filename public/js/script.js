2c0e9e26df94749d74a27bcd7221255384372f77
minhhai205 <158561042+minhhai205@users.noreply.github.com>
2024-08-26 15:42:44 +0700

    add APlayer

--- public/js/script.js
+++ public/js/script.js
@@ -1,1 +1,17 @@
-console.log("OK");
+// Aplayer
+const aplayer = document.querySelector("#aplayer");
+if(aplayer){
+  let dataSong = aplayer.getAttribute("data-song");
+  dataSong = JSON.parse(dataSong);
+
+  const ap = new APlayer({
+    container: aplayer,
+    audio: [{
+        name: dataSong.title,
+        artist: 'artist?',
+        url: dataSong.audio,
+        cover: dataSong.avatar,
+    }]
+  });
+}
+// End Aplayer
