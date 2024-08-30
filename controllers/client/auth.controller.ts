4e43de658682c0f4b3347fa28faacb6d0d63a27e
minhhai205 <158561042+minhhai205@users.noreply.github.com>
2024-08-30 23:56:23 +0700

    user login

--- controllers/client/auth.controller.ts
+++ controllers/client/auth.controller.ts
@@ -6,7 +6,7 @@ import md5 from "md5";
 // [GET] /auth/register
 export const register = async (req: Request, res: Response): Promise<void> => {
   res.render("client/pages/auth/register", {
-    pagesTitle: "Sign up",
+    pageTitle: "Sign up",
   });
 }
 
@@ -19,6 +19,7 @@ export const registerPost = async (req: Request, res: Response): Promise<void> =
     });
 
     if(exitsUser){
+      // add Notification...
       res.send("email already exists");
     }
 
@@ -34,6 +35,52 @@ export const registerPost = async (req: Request, res: Response): Promise<void> =
       res.redirect("/");
     }
   } catch (error) {
+    // add Notification...
     res.send("Registration failed");
+  }
+}
+
+// [GET] /auth/login
+export const login = async (req: Request, res: Response): Promise<void> => {
+  res.render("client/pages/auth/login", {
+    pageTitle: "Sign in",
+  });
+}
+
+// [GET] /auth/login
+export const loginPost = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    const user = await User.findOne({
+      email: email,
+      deleted: false,
+    });
+
+    if(!user){
+      // add Notification...
+      console.log("email does not exist!");
+      res.redirect("back");
+    }
+
+    if(md5(password) != user.password) {
+      // add Notification...
+      res.redirect("back");
+      return;
+    }
+  
+    if(user.status != "active") {
+      // add Notification...
+      res.redirect("back");
+      return;
+    }
+  
+    res.cookie("tokenUser", user.tokenUser);
+  
+    res.redirect("/");
+
+  } catch (error) {
+    res.redirect("back");
   }
 }
